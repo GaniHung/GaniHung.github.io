7ff05493557b8acf2be0879a7b652b65bd2bbe9e
GaniHung <2020703141@qq.com>
2024-12-06 20:18:59 +0800

    Site updated: 2024-12-06 20:18:58

---
+++ js/theme/useaplayer.js
@@ -0,0 +1,7 @@
+# Inject
+# Insert the code to head (before '</head>' tag) and the bottom (before '</body>' tag)
+# 插入代码到头部 </head> 之前 和 底部 </body> 之前
+inject:
+  head:
+  bottom:
+    - '<div class="aplayer no-destroy" data-id="464916877" data-server="netease" data-type="song" data-fixed="true" data-autoplay="true"> </div>'
